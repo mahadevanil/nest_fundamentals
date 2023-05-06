f623dd8b3577d568a382e0b91bb31ea9bf893191
mahadevanil <rhythmhead97@gmail.com>
2023-05-06 20:29:27 +0530

    basics

---
+++ annotations/variables.ts
@@ -0,0 +1,28 @@
+let apples = 5;
+apples = 2;
+let speed: string = "Hello world";
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green", "blue"];
+
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+//Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
